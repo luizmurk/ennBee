56876d2fe1ce63378b905205f18f9e62d31434a7
luizmurk <will4odia@gmai.com>
2021-03-01 04:40:48 +0100

    second commit

---
+++ lib/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+import 'package:ennBee/screens/splash/splash_screen.dart';
+
+// We use name route
+// All our routes will be available here
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+  //SignInScreen.routeName: (context) => SignInScreen(),
+  //ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+  //SignUpScreen.routeName: (context) => SignUpScreen(),
+  //OtpScreen.routeName: (context) => OtpScreen(),
+};
